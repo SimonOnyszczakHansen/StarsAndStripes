ce24d3c7ac5c89e34d956fa28b51cd85f9940254
zbcsioh <zbcsioh@ZBC-S-985Y0>
2022-08-08 13:45:57 +0200

    Add project files.

---
+++ StarsAndStripes/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace StarsAndStripes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                string stjerner = " *   *   *   *   *   * ";
+                string stjerner2 = "   *   *   *   *   *   ";
+         
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Write("                       ");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Write("               ");
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Write("\r\n" + stjerner);
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Write("               ");
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Write("\r\n" + stjerner2);
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Write("               ");
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Write("\r\n" + stjerner);
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Write("               ");
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Write("\r\n" + stjerner2);
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Write("               ");
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Write("\r\n" + stjerner);
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Write("               ");
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Write("\r\n" + stjerner2);
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Write("               ");
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Write("\r\n" + stjerner);
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Write("               ");
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Write("\r\n" + stjerner2);
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Write("               ");
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Write("\r\n" + stjerner);
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Write("               ");
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Write("\r\n                       ");
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Write("               ");
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Write("\r\n                                      ");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Write("\r\n                                      ");
+            Console.Write("\r\n                                      ");
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Write("\r\n                                      ");
+            Console.Write("\r\n                                      ");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Write("\r\n                                      ");
+            Console.Write("\r\n                                      ");
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Write("\r\n                                      ");
+            Console.Write("\r\n                                      ");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Write("\r\n                                      ");
+            Console.Write("\r\n                                      ");
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Write("\r\n                                      ");
+            Console.Write("\r\n                                      ");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Write("\r\n                                      ");
+            Console.Write("\r\n                                      ");
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
